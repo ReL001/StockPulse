d79a40655330c68eef72dbdfa0b5e6e9b3c3a1b8
JerryKing <106170474+ReL001@users.noreply.github.com>
2024-04-20 02:06:14 +0530

    Modifications

--- js/script.js
+++ js/script.js
@@ -15,4 +15,34 @@ document.addEventListener("DOMContentLoaded", function() {
     
     // Add more JavaScript functionality here as needed
   });
+
+  function searchStock() {
+    var stockName = document.getElementById('stock-input').value.trim();
+    if (stockName) {
+      console.log("Search for:", stockName);
+      // Update the API URL with the stockName parameter
+      var apiKey = 'NEWS_API_KEY'; // Replace 'YOUR_API_KEY' with your actual API key
+      var url = 'https://newsapi.org/v2/everything?' +
+        'q=' + stockName + '&' +
+        'from=2024-04-19&' +
+        'sortBy=popularity&' +
+        'apiKey=' + apiKey;
+  
+      var req = new Request(url);
+  
+      fetch(req)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          console.log(data); // Do something with the API response data
+        })
+        .catch(function(error) {
+          console.error('Error fetching data:', error);
+        });
+    } else {
+      alert("Please enter a stock name to search.");
+    }
+  }
+  
   
