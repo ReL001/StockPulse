d629a7ab29195c1e7b2b577bea3171ee69279e45
Edzer Dionido <jeddionido26@gmail.com>
2024-05-04 10:12:37 +0800

    implement routing for home and about page

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php  ?>
+<?php  
+$request = $_SERVER['REQUEST_URI'];
+switch ($request) {
+     case '':
+	 case '/':
+    	require __DIR__ . '/home.php';
+        break;
+        
+     case '/about':
+    	require __DIR__ . '/about.php';
+}
+?>
