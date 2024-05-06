0fc5465ef321e1602f6632151952fe6d261ccd79
Edzer Dionido <jeddionido26@gmail.com>
2024-05-06 15:39:56 +0800

    add toggle password visibility

--- js/registration.js
+++ js/registration.js
@@ -1,4 +1,5 @@
 $(document).ready(()=> {
+
   $("#registration").submit( function (e) {
     e.preventDefault();
 
@@ -14,13 +15,26 @@ $(document).ready(()=> {
 
       success: function(response) {
         $('.invalid-feedback').css('display', 'block');
+        
 
         if(response.status === 1){
-          $('.invalid-feedback').html('<p id="invalid">' + response.message + '</p>');
+          $('#registration')[0].reset();
+          $('.valid-feedback').css('display', 'block');
+          $('.invalid-feedback').css('display', 'none');
+          $('.valid-feedback').html('<p id="valid">' + response.message + '</p>');
         } else {
           $('.invalid-feedback').html('<p id="invalid">' + response.message + '</p>');
         }
       }
     });
   });
-})
+});
+
+const togglePassword = document.querySelector('#checkPassword');
+const password = document.querySelector('#password');
+
+togglePassword.addEventListener('click', function(){
+const type = password.getAttribute("type") === "password" ? "text" : "password";
+password.setAttribute("type", type);
+});
+
