e7eb2aed514aa74ce86b0c1d9d2bddf7bb652d75
JerryKing <106170474+ReL001@users.noreply.github.com>
2024-05-14 00:36:20 +0530

    chore: Material UI Landing Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import * as React from 'react';
+// import Container from '@mui/material/Container';
+// import Typography from '@mui/material/Typography';
+// import Box from '@mui/material/Box';
+// import ProTip from './ProTip';
+// import Copyright from './Copyright';
+// import {Typography, CssBaseline } from '@mui/material';
+import LandingPage from './landing-page/LandingPage';
 
 export default function App() {
   return (
-    <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+    <>
+      <LandingPage />
+    </>
   )
 }
 
