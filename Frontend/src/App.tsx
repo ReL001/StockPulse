c101d68aa21730e56fb779676961a0b28bd02e4d
JerryKing <106170474+ReL001@users.noreply.github.com>
2024-05-15 14:34:44 +0530

    chore: landing page components

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
+import LandingPage from './landing-page/LandingPage'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +11,7 @@ function App() {
   return (
     <>
       <div className='text-red-600'>hello</div>
+      <LandingPage />
     </>
   )
 }
